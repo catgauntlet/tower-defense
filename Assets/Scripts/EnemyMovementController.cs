aa0fab6386df66ceba6122cd68225ef93e726999
Nan Damhuis <nan@marginzero.dev>
2022-05-30 16:05:37 +0200

    implement enemy movement with coroutine

--- Assets/Scripts/EnemyMovementController.cs
+++ Assets/Scripts/EnemyMovementController.cs
@@ -6,18 +6,24 @@ using UnityEngine;
 public class EnemyMovementController : MonoBehaviour
 {
     [SerializeField] List<WaypointController> path = new List<WaypointController>();
+    [SerializeField] private float waitTime = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintWaypointName();
+        print("Initializing wayfinding");
+        StartCoroutine(MoveToWaypoint());
+        print("Finished Start method");
     }
     
-    void PrintWaypointName()
+    IEnumerator MoveToWaypoint()
     {
         foreach( WaypointController waypoint in path)
         {
             print(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
         }
+
     }
 }
